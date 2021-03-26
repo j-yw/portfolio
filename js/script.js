cfb695fba2a6efaa12249b7735e391694f9a95b2
Yiwei J <32629001+j-yw@users.noreply.github.com>
2021-03-27 00:28:27 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function () {
+  emailjs.init("user_0wkILVfPQZdHvtcmmP18V");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_uh0wqxv", "contact_form", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
